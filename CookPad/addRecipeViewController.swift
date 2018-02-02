049de50e75e002d7fb19dd4486ece1b6186963c5
Satkaran Tamber <Satkaran@ucrwpa1-8-10-25-31-108.wnet.ucr.edu>
2018-02-02 10:25:07 -0800

    -Fixed implicit error thingy swift 4

--- CookPad/addRecipeViewController.swift
+++ CookPad/addRecipeViewController.swift
@@ -196,7 +196,7 @@ class addRecipeViewController: UIViewController, UIImagePickerControllerDelegate
     }
     
     //Calls this function when the tap is recognized.
-    func dismissKeyboard() {
+    @objc func dismissKeyboard() {
         //Causes the view (or one of its embedded text fields) to resign the first responder status.
         view.endEditing(true)
     }
