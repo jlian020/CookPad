9fcbe975dbb3e1da8d4a13346d5d178cc239f1bd
Satkaran Tamber <Satkaran@ucrwpa3-8-10-25-95-231.wnet.ucr.edu>
2018-02-14 15:58:31 -0800

    -

--- CookPad/addRecipeViewController.swift
+++ CookPad/addRecipeViewController.swift
@@ -145,7 +145,7 @@ class addRecipeViewController: UIViewController, UIImagePickerControllerDelegate
         let currentUserID = Auth.auth().currentUser?.uid
         print(recipeID?.key) //later assign this key value to myRecipes
         recipeID?.child("Name").setValue(recipeTitleTextField.text)
-        recipeID?.child("Method").setValue(methodTextView.text)
+        recipeID?.child("Directions").setValue(methodTextView.text)
         recipeID?.child("Ingredients").setValue(ingredientsTextView.text)
         var numOfRecipesInFirebase: String!
         myFirebaseNetworkDataRequest {
