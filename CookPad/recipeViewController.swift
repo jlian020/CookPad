5184f87a88a84f8930ab5c269981dc8fe0690c91
Satkaran Tamber <Satkaran@satkarans-macbook-pro.local>
2018-02-25 17:42:12 -0800

    -fixed liking same recipe bug

--- CookPad/recipeViewController.swift
+++ CookPad/recipeViewController.swift
@@ -22,8 +22,9 @@ class recipeViewController: UIViewController {
     let likeOverlay = UIImage(named: "like button")
     var reference : DatabaseReference?
     let currentUserId = Auth.auth().currentUser?.uid
-    var myLikedRecipeDict : NSArray?
+    var myLikedRecipe : NSArray?
     var userNumberOfLikedRecipes: Int! = 0
+    let savedVC = savedRecipeTVC(nibName: "savedRecipeTVC", bundle: nil)
     
     
     override func viewDidLoad()
@@ -35,7 +36,6 @@ class recipeViewController: UIViewController {
         self.nameLabel?.text = self.recipe?.name
         self.ingredientsList?.text = self.recipe?.ingredients
         self.directionsTextView?.text = self.recipe?.directions
-        
         var numDirectionsLines = (directionsTextView.contentSize.height / (directionsTextView.font?.lineHeight)!) as? CGFloat
         var numIngredientsLines = ((ingredientsList?.contentSize.height)! / (ingredientsList?.font?.lineHeight)!) as? CGFloat
         let numOfLines = numDirectionsLines! + numIngredientsLines!
@@ -52,11 +52,29 @@ class recipeViewController: UIViewController {
         //stamp like, hide buttons, save the recipe to saved
         overlayImageView?.isHidden = false
         overlayImageView?.image = likeOverlay
-        myFirebaseNetworkDataRequest {
-            //stuff that is down after the fetch from the database
-            self.reference?.child("Users").child(self.currentUserId!).child("LikedRecipes").child("\(self.userNumberOfLikedRecipes! - 1)").setValue(self.recipe?.firebaseId)
-            self.reference?.child("Users").child(self.currentUserId!).child("numOfLikedRecipes").setValue("\(self.userNumberOfLikedRecipes!)")
+        grabLikedFromFirebase {
+            var alreadyLiked: Bool = false
+            if self.myLikedRecipe != nil {
+                for each in self.myLikedRecipe! {
+                    print("reaches")
+                    if (each as! String) == self.recipe?.firebaseId {
+                        print("print")
+                        alreadyLiked = true
+                    }
+                }
+            }
+            print(alreadyLiked)
+            if alreadyLiked == false {
+                print("got in")
+                self.myFirebaseNetworkDataRequest {
+                    //stuff that is down after the fetch from the database
+                    self.reference?.child("Users").child(self.currentUserId!).child("LikedRecipes").child("\(self.userNumberOfLikedRecipes! - 1)").setValue(self.recipe?.firebaseId)
+                    self.reference?.child("Users").child(self.currentUserId!).child("numOfLikedRecipes").setValue("\(self.userNumberOfLikedRecipes!)")
+                    //self.savedVC.loadSavedRecipes()
+                }
+            }
         }
+
         
         //When pressed, save the recipe to the user's 'Saved Recipes' folder
     }
@@ -82,6 +100,27 @@ class recipeViewController: UIViewController {
             finished()
         })
         
+    }
+    
+    func grabLikedFromFirebase(finished: @escaping () -> Void){ // the function thats going to take a little moment
+        reference?.child("Users").child(currentUserId!).observeSingleEvent(of: .value, with: {(UserRecipeSnap) in
+            if UserRecipeSnap.exists(){
+                if UserRecipeSnap.hasChild("LikedRecipes") {
+                    let Dict : NSDictionary = UserRecipeSnap.value as! NSDictionary
+                    print("entered")
+                    self.myLikedRecipe = Dict["LikedRecipes"] as? NSArray
+                    finished()
+                }
+                else {
+                    print("LikedRecipes DOES NOT EXIST")
+                    finished()
+                }
+            }
+            else {
+                print("Error with retrieving snapshot from Firebase")
+            }
+            
+        })
     }
 
     
