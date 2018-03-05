22796418cf5bffe73e167b9c1afb9010869efdfb
David Liang <davidliang@bruh>
2018-03-04 17:19:47 -0800

    Finished aligning the stars and recipe name. Also removed the description in cell

--- CookPad/savedRecipeTVC.swift
+++ CookPad/savedRecipeTVC.swift
@@ -14,7 +14,6 @@ class savedRecipeCell: UITableViewCell {
         
     @IBOutlet weak var recipeImage: UIImageView!
     @IBOutlet weak var recipeLabel: UILabel!
-    @IBOutlet weak var recipeDescription: UILabel!
     
     @IBOutlet weak var star1: UIImageView!
     @IBOutlet weak var star2: UIImageView!
@@ -34,7 +33,7 @@ class savedRecipeTVC: UITableViewController {
     var recipeImage : UIImage?
     var recipePictureURL : URL?
     var myLikedRecipeDict : NSArray?
-
+    
     let vc = ViewController(nibName: "ViewController", bundle: nil)
     
     
@@ -44,7 +43,7 @@ class savedRecipeTVC: UITableViewController {
         refresh = UIRefreshControl()
         refresh.attributedTitle = NSAttributedString(string: "Pull to load recipes")
         refresh.addTarget(self, action: #selector(savedRecipeTVC.loadSavedRecipes), for: .valueChanged)
-//        self.collectionView.addSubview(refresh) //adds a refresh action to the collectionView so we can update profiles
+        //        self.collectionView.addSubview(refresh) //adds a refresh action to the collectionView so we can update profiles
         self.tableView.addSubview(refresh)
         
         navigationController?.navigationBar.barTintColor = UIColor(red: 197/255, green: 42/255, blue: 53/255, alpha: 0.1) //changes the navigation bar color to light blue, divide by 255 to convert RGB
@@ -52,27 +51,56 @@ class savedRecipeTVC: UITableViewController {
         navigationController?.navigationBar.barStyle = UIBarStyle.black
         navigationController?.navigationBar.tintColor = UIColor.white
         
-         loadSavedRecipes()
+        loadSavedRecipes()
     }
     
     @objc func loadSavedRecipes() -> Void {
         savedRecipes.removeAll()
         refresh.endRefreshing()
-        let storageRef = storage.reference() //create storage reference from Firebase Storage
-        for index in 1...5 {
-            let imageRef = storageRef.child("Images/a\(index).JPG")
-            imageRef.getData(maxSize: 3*1024*1024, completion: { (data, error) in
-                if let error = error {
-                    print("Error: \(error)")
-                } else {
-                    var newRecipe = Recipe.init(name: "test", image: UIImage(data: data!)!, ingredients: ["Stuff"], directions: ["Do Stuff"]);
-                    self.savedRecipes.append(newRecipe)
-                    DispatchQueue.main.async(execute: {
-                        //push the current info into the main thread, otherwise for loop would be asynchronous
-                        // self.collectionView.reloadData() //add new recipe to collectionView
-                        self.tableView.reloadData()
-                        //self.refresh.endRefreshing()
-                    })
+        grabLikedRecipesFromFirebase {
+            print(self.myLikedRecipeDict!)
+            if self.myLikedRecipeDict!.count > 0 {
+                self.grabRecipes {
+                    //HELLO
+                }
+            }
+            
+        }
+        
+    }
+    
+    
+    func grabRecipes(finished: @escaping () -> Void){ // the function thats going to take a little moment
+        for each in self.myLikedRecipeDict!{
+            let x: String = each as! String
+            self.reference?.child("Recipes").child(x).observeSingleEvent(of: .value, with: { (snapshot) in
+                if snapshot.exists() {
+                    let value = snapshot.value as? NSDictionary
+                    while snapshot.childrenCount != 4 {}
+                    let tempRecipeName = value?["Name"] as? String ?? ""
+                    let tempRecipeIngredients = value?["Ingredients"] as? String ?? ""
+                    let tempRecipeDirections = value?["Directions"] as? String ?? ""
+                    var tempRecipeURL = value?["storageURL"] as? String ?? ""
+                    while(tempRecipeURL == "") {
+                        tempRecipeURL = value?["storageURL"] as? String ?? ""
+                    }
+                    
+                    self.recipePictureURL = URL(string: tempRecipeURL)
+                    self.myFirebaseStorageImageGrab {
+                        let newRecipe = Recipe.init(name: tempRecipeName, image: self.recipeImage!, ingredients: tempRecipeIngredients, directions: tempRecipeDirections, id: x)
+                        self.savedRecipes.append(newRecipe)
+                        DispatchQueue.main.async(execute: {
+                            //push the current info into the main thread, otherwise for loop would be asynchronous
+                            if self.vc.recipeDoneSending == true {
+                                self.tableView.reloadData() //add new recipe to collectionView
+                                //self.refresh.endRefreshing()
+                            }
+                        })
+                    }
+                    finished()
+                }
+                else {
+                    print("Error: Snapshot doesnt exist")
                 }
             })
         }
@@ -127,26 +155,26 @@ class savedRecipeTVC: UITableViewController {
         downloadPicTask.resume()
         
     }
-
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     // MARK: - Table view data source
-
+    
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return savedRecipes.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "labelCell", for: indexPath) as! savedRecipeCell
         
         cell.recipeLabel?.text = savedRecipes[indexPath.row].name
         cell.recipeImage?.image = savedRecipes[indexPath.row].image
-        cell.recipeDescription?.text = "BLAH BLAH BLACH BLAH BLACH"
+
         cell.star1?.image = UIImage(named: "star2")
         cell.star2?.image = UIImage(named: "star2")
         cell.star3?.image = UIImage(named: "star2")
@@ -165,7 +193,7 @@ class savedRecipeTVC: UITableViewController {
             let backButton = UIBarButtonItem()
             backButton.title = "" //want an empty title, rather than app name near back button
             navigationItem.backBarButtonItem = backButton //recreates bar button with empty title
-//            let indexPaths = self.collectionView!.indexPathsForSelectedItems! //get the number of selected items in our collectionView
+            //            let indexPaths = self.collectionView!.indexPathsForSelectedItems! //get the number of selected items in our collectionView
             let indexPaths = self.tableView!.indexPathsForSelectedRows!
             let indexPath = indexPaths[0] as IndexPath //start at first i
             
@@ -174,9 +202,9 @@ class savedRecipeTVC: UITableViewController {
             //set the profile view up
             let recipe = savedRecipes[indexPath.row]
             recipeVC.recipe = recipe
-
+            
         }
     }
     
-
+    
 }
